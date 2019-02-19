8998af9d6982fbca3890d096bae56a54b755a8f7
Michał Narajowski <michal.narajowski@codecoup.pl>
2019-02-19 15:34:40 +0100

    Add working example using nv-websocket client

--- app/src/main/java/com/juul/btptesterandroid/MainActivity.java
+++ app/src/main/java/com/juul/btptesterandroid/MainActivity.java
@@ -1,119 +1,108 @@
 package com.juul.btptesterandroid;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.util.Arrays;
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+import com.neovisionaries.ws.client.WebSocketException;
+import com.neovisionaries.ws.client.WebSocketFactory;
+import com.neovisionaries.ws.client.WebSocketFrame;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-import io.socket.client.IO;
-import io.socket.client.Manager;
-import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
-import io.socket.engineio.client.Transport;
-
 public class MainActivity extends AppCompatActivity {
-    private Socket socket = null;
-    private Activity activity = MainActivity.this;
+    /**
+     * The echo server on websocket.org.
+     */
+    private static final String SERVER = "ws://192.168.9.111:8765";
+
+    /**
+     * The timeout value in milliseconds for socket connection.
+     */
+    private static final int TIMEOUT = 5000;
+
+    WebSocket ws = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            socket = IO.socket("http://192.168.9.111:8765");
+        // Create a WebSocket factory and set 5000 milliseconds as a timeout
+        // value for socket connection.
+        WebSocketFactory factory = new WebSocketFactory().setConnectionTimeout(TIMEOUT);
 
-            socket.on(Socket.EVENT_CONNECT, onConnect);
-            socket.on(Socket.EVENT_DISCONNECT, onDisconnect);
-            socket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
-            socket.on("newMessage", newMessageListner);
-//            socket.on(Manager.EVENT_TRANSPORT, onTransport);
-            socket.connect();
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
+        // Create a WebSocket. The timeout value set above is used.
+        try {
+            ws = factory.createSocket(SERVER);
+
+            ws.addListener(adapter);
+
+            ws.connectAsynchronously();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
-//    private Emitter.Listener onTransport = new Emitter.Listener() {
-//        @Override
-//        public void call(final Object... args) {
-//            Log.d("TAG", "onTransport: " + Arrays.toString(args));
-//
-//            Transport transport = (Transport) args[0];
-//            // Adding headers when EVENT_REQUEST_HEADERS is called
-//            transport.on(Transport.EVENT_REQUEST_HEADERS, new Emitter.Listener() {
-//                @Override
-//                public void call(Object... args) {
-//                    Log.v("TAG", "Caught EVENT_REQUEST_HEADERS after EVENT_TRANSPORT, adding headers");
-//                    Map<String, List<String>> mHeaders = (Map<String, List<String>>)args[0];
-//                    mHeaders.put("Authorization", Arrays.asList("Basic bXl1c2VyOm15cGFzczEyMw=="));
-//                }
-//            });
-//        }
-//    };
-
-
-    private Emitter.Listener onConnectError = new Emitter.Listener() {
+    WebSocketAdapter adapter = new WebSocketAdapter() {
         @Override
-        public void call(final Object... args) {
-            Log.d("TAG", "onConnectError: " + Arrays.toString(args));
-        }
-    };
-
-    private Emitter.Listener onDisconnect = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-            if (activity != null)
-                activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Log.d("TAG", "onDisconnect: " + Arrays.toString(args));
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-        }
-    };
-
-    private Emitter.Listener onConnect = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-            Log.d("TAG", "onConnect: " + Arrays.toString(args));
+        public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
+            Log.d("TAG", "onConnected");
+            super.onConnected(websocket, headers);
             sendMessage();
         }
-    };
 
-    private Emitter.Listener newMessageListner = new Emitter.Listener() {
         @Override
-        public void call(final Object... args) {
-            Log.d("TAG", "newMessageListner: " + Arrays.toString(args));
+        public void onConnectError(WebSocket websocket, WebSocketException exception) throws Exception {
+            Log.d("TAG", "onConnectError");
+            super.onConnectError(websocket, exception);
         }
+
+        @Override
+        public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
+            Log.d("TAG", "onDisconnected");
+            super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
+            // Create a new WebSocket instance and connect to the same endpoint.
+            ws = ws.recreate().connect();
+        }
+
+        @Override
+        public void onTextMessage(WebSocket websocket, String message) {
+            Log.d("TAG", "onTextMessage: " + message);
+        }
+
+        @Override
+        public void onBinaryMessage(WebSocket websocket, byte[] binary) throws Exception {
+            Log.d("TAG", "onBinaryMessage");
+            super.onBinaryMessage(websocket, binary);
+        }
+
+        @Override
+        public void onUnexpectedError(WebSocket websocket, WebSocketException cause) throws Exception {
+            Log.d("TAG", "onUnexpectedError");
+            super.onUnexpectedError(websocket, cause);
+        }
+
     };
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        if (socket != null) {
-            socket.disconnect();
-            socket = null;
+        if (ws != null) {
+            ws.disconnect();
+            ws = null;
         }
     }
 
     public void sendMessage() {
-        if (socket.connected()) {
-            socket.emit("newMessage", "Message from Android!");
+        if (ws.isOpen()) {
+            ws.sendText("Message from Android!");
         }
     }
 }
