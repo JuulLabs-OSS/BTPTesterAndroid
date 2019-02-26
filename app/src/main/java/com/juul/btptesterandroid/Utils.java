db32a7dfd09c79e3095fc20031a62e79d41a7d33
Michał Narajowski <michal.narajowski@codecoup.pl>
2019-02-26 15:35:04 +0100

    Add utility functions

--- app/src/main/java/com/juul/btptesterandroid/Utils.java
+++ app/src/main/java/com/juul/btptesterandroid/Utils.java
@@ -27,4 +27,28 @@ public class Utils {
     static byte clearBit(byte b, int bit) {
         return (byte) (b & ~(1 << bit));
     }
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        if (bytes == null) {
+            return "";
+        }
+
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static void reverseBytes(byte[] bytes) {
+        for(int i = 0; i < bytes.length / 2; i++)
+        {
+            byte temp = bytes[i];
+            bytes[i] = bytes[bytes.length - i - 1];
+            bytes[bytes.length - i - 1] = temp;
+        }
+    }
 }
